8e706e03a7c1d240808ab123ecbcb9720d0fdddc
Jake <panamajake86@gmail.com>
2019-11-07 21:59:24 -0700

    I DID IT! And I didn't even need to use a custom hook!

--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import Dashboard from './Dashboard';
-import useStrikes from '../Hooks/useStrikes';
-import useBalls from '../Hooks/useBalls';
 
 const Display = () => {
 
-    const [balls, setBalls] = useBalls(0);
-    const [strikes, setStrikes] = useStrikes(0);
+    const [balls, setBalls] = React.useState(0);
+    const [strikes, setStrikes] = React.useState(0);
 
-    React.useEffect(()=>{
-        (function () {
-            setBalls(balls);
-        })()
+    let ball = () => {
+        if (balls < 3) {
+            setBalls(balls + 1);
+        } else {
+            setStrikes(0);
+            setBalls(0);
+        }
+    };
 
-        (function () {
-            setStrikes(strikes);
-        })()
+    let strike = () => {
+        if (strikes < 2) {
+            setStrikes(strikes + 1);
+        } else {
+            setStrikes(0);
+            setBalls(0);
+        }
+    };
 
-    });
+    let foul = () => {
+        if (strikes < 2) {
+            setStrikes(strikes + 1);
+        } else {
+            setStrikes(strikes + 0);
+        }
+    };
+
+    let hit = () => {
+        setStrikes(0);
+        setBalls(0);
+    };
 
 
 
@@ -32,8 +50,8 @@ const Display = () => {
                 <p>{strikes}</p>
                 <h1>Strikes</h1>
             </div>
-            <Dashboard />
-        </div>
+            <Dashboard ball={ball} strike={strike} foul={foul} hit={hit} />
+        </div> 
     );
 };
 
